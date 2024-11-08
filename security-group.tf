50edab1a5c0b409dc7697ff07db3b7cad394e150
Luiz Carlos Nascimento Junior <thecalifornia16@hotmail.com>
2024-11-07 21:54:43 -0300

    first commit

---
+++ security-group.tf
@@ -0,0 +1,50 @@
+#Configuração do security group para acesso ssh, http e https
+resource "aws_security_group" "bt-avantiSG-app" {
+  name        = "bt-avantiSG-app"
+  description = "Allow incoming HTTP, HTTPS e SSH connections."
+
+  #Inbound
+  ingress {
+    description = "SSH to EC2"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP to EC2"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Porta Site no EC2"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS to EC2"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #Outbound
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "bt-avantiSG-app"
+  }
+}
